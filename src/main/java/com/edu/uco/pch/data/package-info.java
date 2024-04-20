6d91ef16b8bbf2c0ff5fc19b60f7735671b2f548
Juan Jose Arenas <juanjoseaq99@gmail.com>
2024-04-19 20:17:14 -0500

    Creacion de los objetos Pais, Departamento y Ciudad, con sus validaciones

---
+++ src/main/java/com/edu/uco/pch/data/package-info.java
@@ -0,0 +1,1 @@
+package com.edu.uco.pch.data;
---
+++ src/main/java/com/edu/uco/pch/dto/PaisDTO.java
@@ -0,0 +1,41 @@
+package com.edu.uco.pch.dto;
+
+import java.util.UUID;
+
+import com.edu.uco.pch.crosscutting.Helper.TextHelper;
+
+public final class PaisDTO {
+	private UUID id;
+	private String nombre;
+	
+	public PaisDTO() {
+		super();
+		
+	}
+	
+	public PaisDTO(final UUID id, final String nombre) {
+		setId(id);
+		setNombre(nombre);
+	}
+	
+	public static final PaisDTO build() {
+		return new PaisDTO();
+	}
+	
+	public final UUID getId() {
+		return id;
+	}
+	public final PaisDTO setId(final UUID id) {
+		this.id = id;
+		return this;
+	}
+	public final String getNombre() {
+		return nombre;
+	}
+	public final PaisDTO setNombre(final String nombre) {
+		this.nombre = TextHelper.applyTrim(nombre);
+		return this;
+
+	}
+
+}
